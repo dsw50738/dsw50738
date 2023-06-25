2d5c57a0931eb147a54cb3a55216f130ee64d35e
dsw50738 <dsw50738@student.dsw.edu.pl>
2023-06-25 16:07:11 +0200

    branch3

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,17 @@
 if [ $1 == "--date" ];
 then
 date
+elif [ $1 == "--logs" ];
+then
+x=1
+
+while [ $x -le 100 ]
+do
+filename="log${x}.txt"
+touch "$filename"
+echo "Utworzono plik $filename"
+x=$((x+1))
+done
 else
-echo "podałeś zły argument"
+	echo "podaj argument"
 fi
